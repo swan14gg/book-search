a895e3acb89edd87f2279231280b3eca4de29bc3
owsbj <owsbj1011@gmail.com>
2020-12-17 21:55:25 +0900

    create select search type

--- src/components/RootContainer.vue
+++ src/components/RootContainer.vue
@@ -1,27 +1,41 @@
 <template>
   <Header />
-  <SearchBar v-model="searchTarget" />
+  <SearchBar v-model="state.searchTarget" />
+  <SelectSearchType @set="setIsRelevance" />
+  {{ state }}
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent, reactive } from 'vue';
 import Header from './Header.vue';
 import SearchBar from './SearchBar.vue';
+import SelectSearchType from './SelectSearchType.vue';
+
+type State = {
+  searchTarget: string;
+  isRelevance: boolean;
+}
 
 export default defineComponent({
   name: 'RootContainer',
   components: {
     Header,
-    SearchBar
+    SearchBar,
+    SelectSearchType,
   },
+
   setup() {
-    const searchTarget = ref('');
+    const state = reactive<State>({
+      searchTarget: "",
+      isRelevance: true
+    });
+    function setIsRelevance(value: boolean) {
+      state.isRelevance = value;
+    }
     return {
-      searchTarget
+      state,
+      setIsRelevance
     }
   },
 });
 </script>
-
-<style scoped lang="scss">
-</style>
