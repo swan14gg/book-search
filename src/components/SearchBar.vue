fed9ba4626be022a38ba6cb197b0c25bb69f7dd3
owsbj <owsbj1011@gmail.com>
2020-12-17 11:42:22 +0900

    update search bar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,5 +1,19 @@
 <template>
-  <input type="text" :value="modelValue" @input="updateValue">
+  <form>
+    <div class="field has-addons">
+      <div class="control has-icons-left">
+        <input type="search" class="input" :class="[commonClasses]" placeholder="Search books..." :value="modelValue" @input="updateValue">
+        <span class="icon is-left">
+          <i class="fas fa-book"></i>
+        </span>
+      </div>
+      <div class="control">
+        <button type="submit" class="button" :class="[commonClasses]">
+          <i class="fas fa-search"></i>
+        </button>
+      </div>
+    </div>
+  </form>
 </template>
 
 <script lang="ts">
@@ -11,11 +25,15 @@ export default defineComponent({
     modelValue: String
   },
   setup(props, context) {
+    const commonClasses = [
+      "is-medium", "is-rounded", "is-primary"
+    ]
     function updateValue(event: InputEvent) {
       if (!(event.target instanceof HTMLInputElement)) return;
       context.emit("update:modelValue", event.target.value);
     }
     return {
+      commonClasses,
       updateValue
     }
   }
