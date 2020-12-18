fc4885230e9c4164ae85ded12cd1b245d764dfd1
owsbj <owsbj1011@gmail.com>
2020-12-18 18:31:39 +0900

    use optional chaining

--- src/composables/use-action-bookinfos.ts
+++ src/composables/use-action-bookinfos.ts
@@ -5,12 +5,12 @@ import { fetchBookApiDatas } from '@/api';
 export default function(bookInfos: Ref<BookInfo[]>) {
   async function getBookInfos(target: string, isByRelevance: boolean) {
     const bookApiDatas = await fetchBookApiDatas(target, isByRelevance);
-    bookInfos.value = bookApiDatas.map((data) => {
+    bookInfos.value = bookApiDatas.map((data: any) => {
       return {
         title: data.volumeInfo.title,
         authors: data.volumeInfo.authors,
         description: data.volumeInfo.description,
-        thumbnail: data.volumeInfo.imageLinks.thumbnail,
+        thumbnail: data.volumeInfo.imageLinks?.thumbnail,
         previewLink: data.volumeInfo.previewLink,
       };
     });
