7c565c999ab6f141dc246461d6e87eff7cab874f
owsbj <owsbj1011@gmail.com>
2020-12-18 18:07:41 +0900

    fix url

--- src/api/index.ts
+++ src/api/index.ts
@@ -8,7 +8,8 @@ const orderBy = '&orderBy=';
 const maxResults = '&maxResults=40';
 
 export async function fetchBookApiDatas(target: string, isByRelevance: boolean) {
-  const url = baseUrl + target + orderBy + isByRelevance ? 'relevance' : 'newest' + maxResults;
+  const url = baseUrl + target + orderBy + (isByRelevance ? 'relevance' : 'newest') + maxResults;
+  console.log(url);
   const { data } = await axios.get<ApiData>(url);
   return data.items;
 }
