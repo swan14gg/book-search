d06a655e61f4d8ca6d59b39c71a0d3b8bedff133
owsbj <owsbj1011@gmail.com>
2020-12-18 22:28:03 +0900

    add type

--- src/composables/use-action-bookinfos.ts
+++ src/composables/use-action-bookinfos.ts
@@ -5,15 +5,25 @@ import { fetchBookApiDatas } from '@/api';
 export default function(bookInfos: Ref<BookInfo[]>) {
   async function getBookInfos(target: string, isByRelevance: boolean) {
     const bookApiDatas = await fetchBookApiDatas(target, isByRelevance);
-    bookInfos.value = bookApiDatas.map((data: any) => {
-      return {
-        title: data.volumeInfo.title,
-        authors: data.volumeInfo.authors,
-        description: data.volumeInfo.description,
-        thumbnail: data.volumeInfo.imageLinks?.thumbnail,
-        previewLink: data.volumeInfo.previewLink,
-      };
-    });
+    bookInfos.value = bookApiDatas.map(
+      (data: {
+        volumeInfo: {
+          title: string;
+          authors: string[];
+          description: string;
+          imageLinks: { thumbnail: string };
+          previewLink: string;
+        };
+      }) => {
+        return {
+          title: data.volumeInfo.title,
+          authors: data.volumeInfo.authors,
+          description: data.volumeInfo.description,
+          thumbnail: data.volumeInfo.imageLinks?.thumbnail,
+          previewLink: data.volumeInfo.previewLink,
+        };
+      }
+    );
   }
   return {
     getBookInfos,
