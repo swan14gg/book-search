8ff1929301806ff78470024881ff608f54322aaa
owsbj <owsbj1011@gmail.com>
2020-12-18 15:39:56 +0900

    create api process

---
+++ src/api/index.ts
@@ -0,0 +1,14 @@
+import { ApiData } from '@/types';
+import axios from 'axios';
+
+const baseUrl = 'https://www.googleapis.com/books/v1/volumes?q=search+';
+
+// parameters
+const orderBy = '&orderBy=';
+const maxResults = '&maxResults=40';
+
+export async function getBookInfos(target: string, isByRelevance: boolean) {
+  const url = baseUrl + target + orderBy + isByRelevance ? 'relevance' : 'newest' + maxResults;
+  const { data } = await axios.get<ApiData>(url);
+  return data.items;
+}
