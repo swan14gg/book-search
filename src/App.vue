e2d7c4e459c889b6b1f9deb87d07cb9bdb2a1e56
owsbj <owsbj1011@gmail.com>
2020-12-20 14:37:21 +0900

    format

--- src/App.vue
+++ src/App.vue
@@ -6,18 +6,17 @@
 import { defineComponent } from 'vue';
 import RootContainer from './components/RootContainer.vue';
 
-
 export default defineComponent({
   name: 'App',
   components: {
     RootContainer,
-  }
+  },
 });
 </script>
 
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@400;700&display=swap');
-@import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
+@import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
 
 $family-sans-serif: 'M PLUS Rounded 1c', sans-serif;
 
@@ -26,7 +25,7 @@ $family-sans-serif: 'M PLUS Rounded 1c', sans-serif;
   -moz-osx-font-smoothing: grayscale;
 }
 
-@import "../node_modules/bulma/bulma.sass";
+@import '../node_modules/bulma/bulma.sass';
 
 html {
   font-size: 12px;
