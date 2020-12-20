dee0330dcba0bc8723281c4a7b7765a35bd6cdee
owsbj <owsbj1011@gmail.com>
2020-12-20 15:50:35 +0900

    fix action

--- src/composables/use-action-bookinfos.ts
+++ src/composables/use-action-bookinfos.ts
@@ -5,7 +5,7 @@ import { fetchBookApiDatas } from '@/api';
 export default function(bookInfos: Ref<BookInfo[]>) {
   async function getBookInfos(target: string, isByRelevance: boolean) {
     const bookApiDatas = await fetchBookApiDatas(target, isByRelevance);
-    if (!bookApiDatas) return [];
+    if (!bookApiDatas) return;
     bookInfos.value = bookApiDatas.map(
       (data: {
         id: string;
