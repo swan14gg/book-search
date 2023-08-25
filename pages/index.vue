6fec69f385bea7c440dd5092b161c0cf5a496fea
swan14gg <owsbj1011@gmail.com>
2023-08-26 00:35:16 +0900

    move RootContainer to index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,56 @@
+<script setup lang="ts">
+const { searchItem } = useSearchItem();
+const { setIsByRelevance } = useActionSearchItem(searchItem);
+const { result } = useResult();
+const { getBookInfos } = useActionResult(result);
+const { appState } = useAppState();
+const { setIsLoading, setOccuredError } = useActionAppState(appState);
+
+function updateBookInfos() {
+  if (appState.isLoading) return;
+  const trimmedTarget = searchItem.searchTarget.trim();
+  if (trimmedTarget === "") return;
+  searchItem.searchTarget = trimmedTarget;
+  setIsLoading(true);
+  getBookInfos(searchItem.searchTarget, searchItem.isByRelevance)
+    .then(() => {
+      setIsLoading(false);
+      setOccuredError(false);
+    })
+    .catch(() => {
+      setIsLoading(false);
+      setOccuredError(true);
+    });
+}
+
+function setSearchType(value: boolean) {
+  setIsByRelevance(value);
+  if (result.searchTarget === "") return;
+  searchItem.searchTarget = result.searchTarget;
+  updateBookInfos();
+}
+</script>
+
 <template>
-  <RootContainer />
+  <Header class="mb-5" />
+  <main class="container is-max-desktop">
+    <SearchBar
+      class="mb-5"
+      v-model="searchItem.searchTarget"
+      @search="updateBookInfos"
+    />
+    <SelectSearchType class="mb-5" @set="setSearchType" />
+    <ErrorMessage
+      v-if="result.isNotFound"
+      :message="`「${result.searchTarget}」に一致する書籍は見つかりませんでした。`"
+    />
+    <ErrorMessage
+      v-if="appState.occuredError"
+      message="エラーが発生しました。再度お試しください。"
+    />
+    <div v-show="appState.isLoading" class="columns is-centered is-mobile">
+      <LoadingIcon />
+    </div>
+    <BookCardList :bookInfos="result.bookInfos" />
+  </main>
 </template>
